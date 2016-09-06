330f1fadba49ea958fe9c10c8baede08a97c0459
Parkminji <alswl775@naver.com>
2016-09-06 11:19:55 +0900

    dddd

---
+++ app/src/main/java/com/example/a512/min/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.a512.min;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button btName;
+    public TextView txName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txName = (TextView)findViewById(R.id.txName);
+        btName = (Button)findViewById(R.id.btName);
+        btName.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                txName.setText("Cho");
+                Toast.makeText(getApplicationContext(), "Cho", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
